44188cae827e500a07f190c73220a3a358dd5c76
Open Risk <openrisk@outlook.com>
2021-03-13 12:11:28 +0100

    correct filename in load fixtures script

--- loadfixtures.sh
+++ loadfixtures.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-NAME[1]="npl_portfolio"
+NAME[1]="synthetic_data_1"
 
 source venv/bin/activate
 for i in ${NAME[@]}
 do
-python3 manage.py loaddata --format=json $i  $i/fixtures/$i.json
+python3 manage.py loaddata --format=json ./npl_portfolio/fixtures/$i.json
 done
-
