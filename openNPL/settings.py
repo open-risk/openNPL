7fae643a810db8e6b37e65652416ad6464e7c61d
Open Risk <openrisk@outlook.com>
2026-05-18 13:38:05 +0200

    fix jazzmin dark/light mismatch; closes #29

--- openNPL/settings.py
+++ openNPL/settings.py
@@ -27,6 +27,7 @@ Django settings for the openNPL platform.
 import os
 
 from django.utils.translation import gettext_lazy as _
+from openNPL.jazzmin_settings import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@e=)h@ikk@p6an)s-e_wpt57p6%h%g$*ra#*g+xg2xr+9xd9tg'
@@ -77,7 +78,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'openNPL.urls'
 ROOT_VIEW = "http://localhost:8001"
 SITE_ID = 1
-SITE_URL = "http://127.0.0.1:8001/"
+# SITE_URL = "http://127.0.0.1:8001/"
+SITE_URL = "http://localhost:8080/"
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
 MEDIA_URL = '/uploads/'
 
@@ -111,6 +114,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.contrib.gis.db.backends.postgis',
+    #     'NAME': 'opennpl',
+    #     'USER': 'opennpluser',
+    #     'PASSWORD': 'opennpluser',
+    #     'HOST': 'localhost',
+    #     'PORT': '5433',
+    # }
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
@@ -146,182 +157,16 @@ LOCALE_PATHS = (
 )
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Amsterdam'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+# Debug-Toolbar
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-JAZZMIN_SETTINGS = {
-    # Whether to show the UI customizer on the sidebar
-    # "show_ui_builder": False,
-    "show_ui_builder": False,
-
-    # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "openNPL",
-
-    # Title on the brand, and login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "openNPL",
-
-    # Title on the brand, and login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "openNPL",
-
-    # square logo to use for your site, must be present in static files, used for favicon and brand on top left
-    "site_logo": "start/img/opennpl.png",
-
-    # Welcome text on the login screen
-    "welcome_sign": "Welcome to openNPL",
-
-    # Copyright on the footer
-    "copyright": "Open Risk",
-
-    # Field name on user model that contains avatar image
-    "user_avatar": None,
-
-    ############
-    # Top Menu #
-    ############
-
-    # Links to put along the top menu
-    "topmenu_links": [
-
-        # Url that gets reversed (Permissions can be added)
-        {"name": "Home", "url": "/", "permissions": ["auth.view_user"]},
-
-        # Url that gets reversed (Permissions can be added)
-        # {"name": "Dashboard", "url": "admin:index", "permissions": ["auth.view_user"]},
-
-        # external url that opens in a new window (Permissions can be added)
-        # {"name": "Support", "url": "https://github.com/open-risk/equinox", "new_window": True},
-
-        # model admin to link to (Permissions checked against model)
-        # {"model": "auth.User"},
-
-        # App with dropdown menu to all its models pages
-        # {"app": "portfolio"},
-
-        # Url that gets reversed (Permissions can be added)
-        # {"name": "Shape", "url": "/asset_manager", "permissions": ["auth.view_user"]},
-
-        # Url that gets reversed (Permissions can be added)
-        # {"name": "Analyse", "url": "/asset_manager", "permissions": ["auth.view_user"]},
-
-    ],
-
-    #############
-    # User Menu #
-    #############
-
-    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
-    "usermenu_links": [
-        {"name": "Support", "url": "https://github.com/open-risk/openNPL", "new_window": True},
-        {"model": "auth.user"}
-    ],
-
-    #############
-    # Side Menu #
-    #############
-
-    # Whether to display the side menu
-    "show_sidebar": True,
-
-    # Whether to expand the side menu
-    "navigation_expanded": False,
-
-    # Hide these apps when generating side menu e.g (auth)
-    "hide_apps": ['auth'],
-
-    # Hide these models when generating side menu (e.g auth.user)
-    "hide_models": [],
-
-    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
-    "order_with_respect_to": ["start", "portfolio", "policy", "risk", "reporting", "auth"],
-
-    # Custom links to append to app groups, keyed on app name
-    # "custom_links": {
-    #     "books": [{
-    #         "name": "Make Messages",
-    #         "url": "make_messages",
-    #         "icon": "fas fa-comments",
-    #         "permissions": ["books.view_book"]
-    #     }]
-    # },
-
-    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
-    # for the full list of 5.13.0 free icon classes
-    "icons": {
-        "auth": "fas fa-users-cog",
-        "auth.user": "fas fa-user",
-        "auth.Group": "fas fa-users",
-    },
-    # Icons that are used when one is not manually specified
-    "default_icon_parents": "fas fa-chevron-circle-right",
-    "default_icon_children": "fas fa-circle",
-
-    #################
-    # Related Modal #
-    #################
-    # Use modals instead of popups
-    "related_modal_active": False,
-
-    #############
-    # UI Tweaks #
-    #############
-    # Relative paths to custom CSS/JS scripts (must be present in static files)
-    "custom_css": "./start/css/jazzmin.css",
-    "custom_js": None,
-
-    ###############
-    # Change view #
-    ###############
-    # Render out the change view as a single form, or in tabs, current options are
-    # - single
-    # - horizontal_tabs (default)
-    # - vertical_tabs
-    # - collapsible
-    # - carousel
-    "changeform_format": "horizontal_tabs",
-    # override change forms on a per modeladmin basis
-    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
-    # Add a language dropdown into the admin
-    "language_chooser": False
-
-}
-
-JAZZMIN_UI_TWEAKS = {
-    "navbar_small_text": False,
-    "footer_small_text": False,
-    "body_small_text": True,
-    "brand_small_text": False,
-    "brand_colour": "False",
-    "accent": "accent-warning",
-    "navbar": "navbar-navy navbar-dark",
-    "no_navbar_border": False,
-    "navbar_fixed": True,
-    "layout_boxed": False,
-    "footer_fixed": False,
-    "sidebar_fixed": False,
-    "sidebar": "sidebar-dark-warning",
-    "sidebar_nav_small_text": True,
-    "sidebar_disable_expand": False,
-    "sidebar_nav_child_indent": False,
-    "sidebar_nav_compact_style": True,
-    "sidebar_nav_legacy_style": False,
-    "sidebar_nav_flat_style": False,
-    "theme": "slate",
-    "dark_mode_theme": "slate",
-    "button_classes": {
-        "primary": "btn-primary",
-        "secondary": "btn-secondary",
-        "info": "btn-outline-info",
-        "warning": "btn-outline-warning",
-        "danger": "btn-outline-danger",
-        "success": "btn-outline-success"
-    }
-}
